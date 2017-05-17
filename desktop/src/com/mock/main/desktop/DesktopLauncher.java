612ee9813f28fcedde1e75f48eb8791470c6cf1d
Trever Mock <Trever Mock>
2017-05-16 21:04:59 -0600

    initial commit - libGDX hello world

---
+++ desktop/src/com/mock/main/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mock.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mock.main.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Game(), config);
+	}
+}
